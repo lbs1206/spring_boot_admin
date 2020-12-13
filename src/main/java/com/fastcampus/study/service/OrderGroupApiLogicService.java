9014eeacd1198102ee6f23cb14f52281c1245623
lbs1206 <qudtjr1993@nate.com>
2020-12-13 22:02:57 +0900

    12-13:주문 API

---
+++ src/main/java/com/fastcampus/study/service/OrderGroupApiLogicService.java
@@ -0,0 +1,107 @@
+package com.fastcampus.study.service;
+
+import com.fastcampus.study.ifs.CrudInterface;
+import com.fastcampus.study.model.entity.Item;
+import com.fastcampus.study.model.entity.OrderGroup;
+import com.fastcampus.study.model.network.Header;
+import com.fastcampus.study.model.network.request.ItemApiRequest;
+import com.fastcampus.study.model.network.request.OrderGroupApiRequest;
+import com.fastcampus.study.model.network.response.ItemApiResponse;
+import com.fastcampus.study.model.network.response.OrderGroupApiResponse;
+import com.fastcampus.study.repository.OrderGroupRepository;
+import com.fastcampus.study.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    private final UserRepository userRepository;
+
+    private final OrderGroupRepository orderGroupRepository;
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+        
+        OrderGroupApiRequest body = request.getData();
+        
+        OrderGroup orderGroup = OrderGroup.builder()
+                .id(body.getId())
+                .status(body.getStatus())
+                .orderType(body.getOrderType())
+                .revAddress(body.getRevAddress())
+                .revName(body.getRevName())
+                .paymentType(body.getPaymentType())
+                .totalPrice(body.getTotalPrice())
+                .totalQuantity(body.getTotalQuantity())
+                .orderAt(body.getOrderAt())
+                .arrivalDate(body.getArrivalDate())
+                .user(userRepository.getOne(body.getUserId()))
+                .build();
+        
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+        
+        return response(newOrderGroup);
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> response(orderGroup))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+        OrderGroupApiRequest body = request.getData();
+
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+                    orderGroup
+                            .setStatus(body.getStatus())
+                            .setOrderType(body.getOrderType())
+                            .setRevAddress(body.getRevAddress())
+                            .setRevName(body.getRevName())
+                            .setPaymentType(body.getPaymentType())
+                            .setTotalPrice(body.getTotalPrice())
+                            .setTotalQuantity(body.getTotalQuantity())
+                            .setOrderAt(body.getOrderAt())
+                            .setArrivalDate(body.getArrivalDate());
+
+                    return orderGroup;
+                })
+                .map(orderGroup -> orderGroupRepository.save(orderGroup))
+                .map(item -> response(item))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+                    return Header.OK();
+                })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+    }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup){
+
+        OrderGroupApiResponse body = OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate())
+                .userId(orderGroup.getUser().getId())
+                .build();
+
+        return Header.OK(body);
+    }
+}
