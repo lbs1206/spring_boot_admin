250316f802705e68626dfa1e472197c89dee2d5a
lbs1206 <qudtjr1993@nate.com>
2020-12-13 21:26:22 +0900

    12-13:상품 API

---
+++ src/main/java/com/fastcampus/study/model/network/request/ItemApiRequest.java
@@ -0,0 +1,39 @@
+package com.fastcampus.study.model.network.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemApiRequest {
+
+    private Long id;
+
+    private String status;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private BigDecimal price;
+
+    @JsonProperty("brand_name")
+    private String brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    @JsonProperty("partner_id")
+    private Long partnerId;
+}
---
+++ src/main/java/com/fastcampus/study/model/network/response/ItemApiResponse.java
@@ -0,0 +1,40 @@
+package com.fastcampus.study.model.network.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemApiResponse {
+
+    private Long id;
+
+    private String status;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private BigDecimal price;
+
+    @JsonProperty("brand_name")
+    private String brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    @JsonProperty("partner_id")
+    private Long partnerId;
+
+}
